cda18196351efe16d264e4e7223a7439e74d2718
teaArchivist <99051511+teaArchivist@users.noreply.github.com>
2023-06-16 13:59:44 -0400

    Added footer and Github url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="App">
     Weather App
+    <footer>
+      This project was coded by <a href="https://github.com/teaArchivist">Alexandra Curran</a> and is open-sourced on
+    <a href="https://deluxe-syrniki-c8616f.netlify.app" target="_blank"> Github</a>.
+    </footer>
     </div>
   );
 }
