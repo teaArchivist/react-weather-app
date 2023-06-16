e96df92a6786b12537b2996f0e48cf28e748dd83
teaArchivist <99051511+teaArchivist@users.noreply.github.com>
2023-06-16 15:20:52 -0400

    Added bootstrap and ran npm update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 export default function App() {
   return (
+    <div className="container">
     <div className="App">
     Weather App
     <footer>
       This project was coded by <a href="https://beamish-wisp-000090.netlify.app">Alexandra Curran</a> and is open-sourced on
     <a href="https://github.com/teaArchivist/react-weather-app" target="_blank"> Github</a>.
     </footer>
+    </div>
     </div>
   );
 }
