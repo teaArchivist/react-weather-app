c75e660d139fbf1b80bdb96a07a66489456b4fee
teaArchivist <99051511+teaArchivist@users.noreply.github.com>
2023-06-29 13:15:02 -0400

    Added five day forecast loop

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,16 @@ if (loaded) {
     return (
     <div className="WeatherForecast">
             <div className="row">
-                <div className="col">
-                    <WeatherForecastDay data={forecast[0]} />
+                {forecast.map(function(dailyForecast, index){
+                    if (index < 5) {
+                    return (
+                    <div className="col" key={index}>
+                    <WeatherForecastDay data={dailyForecast} />
                 </div>
+                    )
+                }
+                })}
+                
             </div>
         </div>
     )
